980b3bf8639fa964e0f7413e3887de9b2da29900
Muhammad Ibrahim <116669316+Muhammadibra40@users.noreply.github.com>
2024-09-15 15:59:12 +0300

    [619] [Time Beats: 93.24%] - LeetPush

---
+++ SQL-50/619-Biggest-Single-Number.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT COALESCE(MAX(num), null) AS num 
+FROM (  SELECT num
+        FROM MyNumbers
+        GROUP BY num
+        HAVING COUNT(num) = 1) AS singleNums;
